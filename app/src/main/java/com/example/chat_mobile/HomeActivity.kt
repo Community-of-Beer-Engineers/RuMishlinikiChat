8ee1517c15e0ca87765a2bc5decc252841f8e57e
aqude <aquadefin@gmail.com>
2023-03-06 23:46:01 +0300

    нужно исправить бля

--- app/src/main/java/com/example/chat_mobile/HomeActivity.kt
+++ app/src/main/java/com/example/chat_mobile/HomeActivity.kt
@@ -2,10 +2,44 @@ package com.example.chat_mobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.chat_mobile.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //TODO
+        // Нужно исправить переходы
+
+        // Replace the initial fragment here
+        replaceFragment(HomeActivity)
+
+        binding.bottomPanelNavBar.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.forum -> replaceFragment(ForumListActivity)
+                R.id.chats -> replaceFragment(ChatFragment())
+                R.id.other -> replaceFragment(OtherFragment())
+                R.id.setting -> replaceFragment(SettingFragment())
+
+                else -> {
+
+                }
+            }
+            true
+        }
+    }
+
+    // Replace the current fragment with a new fragment
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
     }
 }
